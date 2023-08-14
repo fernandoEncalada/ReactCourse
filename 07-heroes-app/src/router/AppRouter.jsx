133f84c76be081e0af5bcc3f1da929498623d3f4
user <ferencalada1@gmail.com>
2023-08-13 21:06:56 -0500

    add heroes app project

--- 07-heroes-app/src/router/AppRouter.jsx
+++ 07-heroes-app/src/router/AppRouter.jsx
@@ -1,16 +1,15 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
-import { MarvelPage } from '../heroes/pages/MarvelPage'
-import { DcPage } from '../heroes/pages/DcPage'
-import { LoginPage } from '../auth/pages/LoginPage'
+
+import { LoginPage } from '../auth';
+import { Navbar } from '../ui';
+import { HeroesRoutes } from '../heroes';
 
 export const AppRouter = () => {
   return (
     <>
         <Routes>
-            <Route path='marvel' element={ <MarvelPage /> }></Route>
-            <Route path='dc' element={ <DcPage /> }></Route>
             <Route path='login' element={ <LoginPage /> }></Route>
-            <Route path='/*' element={ <Navigate to='/marvel' /> }></Route>
+            <Route path='/*' element={ <HeroesRoutes /> }></Route>
         </Routes>
     </>
   )
