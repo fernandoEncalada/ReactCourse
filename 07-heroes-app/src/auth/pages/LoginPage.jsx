0be037b2cf0a02cd6c94407a5aa2f6f98cdf79fe
user <ferencalada1@gmail.com>
2023-08-19 18:29:31 -0500

    finish heroes app

--- 07-heroes-app/src/auth/pages/LoginPage.jsx
+++ 07-heroes-app/src/auth/pages/LoginPage.jsx
@@ -1,10 +1,16 @@
-import {useNavigate} from 'react-router-dom';
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
 
 export const LoginPage = () => {
+
+  const { login } = useContext(AuthContext);
 
   const navigate = useNavigate();
 
   const onLogin = () => {
+    login('José Encalada');
+
     navigate('/', {
       replace: true
     });
@@ -15,7 +21,7 @@ export const LoginPage = () => {
       <h1>Login</h1>
       <hr />
 
-      <button className="btn btn-primary" onClick={ onLogin }>
+      <button className="btn btn-primary" onClick={onLogin}>
         Login
       </button>
     </div>
--- 07-heroes-app/src/router/AppRouter.jsx
+++ 07-heroes-app/src/router/AppRouter.jsx
@@ -3,14 +3,29 @@ import { Routes, Route, Navigate } from 'react-router-dom'
 import { LoginPage } from '../auth';
 import { Navbar } from '../ui';
 import { HeroesRoutes } from '../heroes';
+import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
 
 export const AppRouter = () => {
   return (
     <>
-        <Routes>
-            <Route path='login' element={ <LoginPage /> }></Route>
-            <Route path='/*' element={ <HeroesRoutes /> }></Route>
-        </Routes>
+      <Routes>
+        {/* <Route path='login' element={<LoginPage />}></Route> */}
+
+        <Route path='/login' element={
+          <PublicRoute>
+            <LoginPage />
+          </PublicRoute>
+        } />
+
+        <Route path='/*' element={
+          <PrivateRoute>
+            <HeroesRoutes />
+          </PrivateRoute>
+        } />
+
+        {/* <Route path='/*' element={ <HeroesRoutes /> }></Route> */}
+      </Routes>
     </>
   )
 }
