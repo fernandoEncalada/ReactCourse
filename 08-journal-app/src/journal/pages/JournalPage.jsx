66d9c588ccd33a90722a093ed12b06006c038ee6
fernandoEncalada <ferencalada1@gmail.com>
2023-08-23 23:48:30 -0500

    add designs

--- 08-journal-app/src/journal/pages/JournalPage.jsx
+++ 08-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,12 +1,29 @@
-import { JournalLayout } from "../layout/JournalLayout"
-import { NothingSelectedView } from "../views"
+import { IconButton } from "@mui/material";
+import { JournalLayout } from "../layout/JournalLayout";
+import { NothingSelectedView } from "../views";
+import { NoteView } from "../views/NoteView";
+import { AddOutlined } from "@mui/icons-material";
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
       {/* Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta quae officia, molestias placeat quaerat culpa et animi amet dolor nisi assumenda a aliquam perspiciatis ab! Facere, voluptates? Repudiandae, repellendus. Perspiciatis. */}
-      <NothingSelectedView />
-    
+      {/* <NothingSelectedView /> */}
+      <NoteView />
+
+      <IconButton
+        size="large"
+        sx={{
+          color: "white",
+          backgroundColor: "error.main",
+          ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
     </JournalLayout>
-  )
-}
+  );
+};
